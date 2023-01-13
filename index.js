2e0ac30a6462bf5f2aa96198cc329c36da343485
Sheila-a <ugwuodo20@gmail.com>
2023-01-13 16:57:08 +0100

    Landing Page 99%

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+// theme settings
 var iccon = document.getElementById("iccon");
 
 iccon.onclick = function () {
   document.body.classList.toggle("light-theme");
   if (document.body.classList.contains("light-theme")) {
     iccon.src = "assets/icons/moon.png";
-    // iccon.style = "background-color: white";
   } else {
     iccon.src = "assets/icons/sun.png";
     iccon.style = "background-color: none";
   }
 };
+
+// accordion
+var acc = document.getElementsByClassName("FAQ-list");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
