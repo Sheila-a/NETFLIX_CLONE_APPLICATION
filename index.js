feed51561f4633bce0eedd81ef1dde1ee7ce04c4
Sheila-a <ugwuodo20@gmail.com>
2023-01-15 13:28:11 +0100

    Landing responsive test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ iccon.onclick = function () {
   }
 };
 
-// accordion
+// faq dropdown
 var acc = document.getElementsByClassName("FAQ-list");
 var i;
 
@@ -26,3 +26,31 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// slider
+var homeHeroFlexInner = document.getElementById("home-hero-flex-inner");
+var slidess = homeHeroFlexInner.getElementsByClassName("hero-slides");
+function next() {
+  homeHeroFlexInner.append(slidess[0]);
+}
+// var currentIndex = 1;
+// displaySlides(currentIndex);
+
+// function setSlides(num) {
+//   displaySlides((currentIndex += num));
+// }
+
+// function displaySlides(num) {
+//   var k;
+//   var slides = document.getElementsByClassName("hero-slides");
+//   if (num > slides.length) {
+//     currentIndex = 1;
+//   }
+//   if (num < 1) {
+//     currentIndex = slides.length;
+//   }
+//   for (k = 0; k < slides.length; k++) {
+//     slides[k].style.display = "none";
+//   }
+//   slides[currentIndex - 1].style.display = "block";
+// }
