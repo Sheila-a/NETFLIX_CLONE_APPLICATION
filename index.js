706b1dc1ac9a6c5c4b36810169b448a92f713598
Sheila-a <ugwuodo20@gmail.com>
2023-01-15 19:24:51 +0100

    Hamburger test abort material icon

--- index.js
+++ index.js
@@ -27,30 +27,61 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-// slider
+// slider 1
 var homeHeroFlexInner = document.getElementById("home-hero-flex-inner");
 var slidess = homeHeroFlexInner.getElementsByClassName("hero-slides");
 function next() {
   homeHeroFlexInner.append(slidess[0]);
 }
-// var currentIndex = 1;
-// displaySlides(currentIndex);
 
-// function setSlides(num) {
-//   displaySlides((currentIndex += num));
+// slider 2
+var homeHeroFlexInner2 = document.getElementById("home-hero-flex-inner2");
+var slidess = homeHeroFlexInner2.getElementsByClassName("hs2");
+function next2() {
+  homeHeroFlexInner2.append(slidess[0]);
+}
+
+// slider 3
+var homeHeroFlexInner3 = document.getElementById("home-hero-flex-inner2");
+var slidess = homeHeroFlexInner3.getElementsByClassName("hs2");
+function next3() {
+  homeHeroFlexInner3.append(slidess[0]);
+}
+
+// hamburger
+// const menu = document.querySelector(".menu");
+// const menuItems = document.querySelectorAll(".menuItem");
+// const hamburger = document.querySelector(".hamburger");
+// const closeIcon = document.querySelector(".closeIcon");
+// const menuIcon = document.querySelector(".menuIcon");
+
+// function toggleMenu() {
+//   if (menu.classList.contains("showMenu")) {
+//     menu.classList.remove("showMenu");
+//     closeIcon.style.display = "none";
+//     menuIcon.style.display = "block";
+//   } else {
+//     menu.classList.add("showMenu");
+//     closeIcon.style.display = "block";
+//     menuIcon.style.display = "none";
+//   }
 // }
 
-// function displaySlides(num) {
-//   var k;
-//   var slides = document.getElementsByClassName("hero-slides");
-//   if (num > slides.length) {
-//     currentIndex = 1;
-//   }
-//   if (num < 1) {
-//     currentIndex = slides.length;
-//   }
-//   for (k = 0; k < slides.length; k++) {
-//     slides[k].style.display = "none";
-//   }
-//   slides[currentIndex - 1].style.display = "block";
-// }
+// hamburger.addEventListener("click", toggleMenu);
+
+// menuItems.forEach(function (menuItem) {
+//   menuItem.addEventListener("click", toggleMenu);
+// });
+// window.onload = function () {
+//   // window.addEventListener("scroll", function(e){
+//   //   if(window.scrollX > 100){
+//   //     this.document.querySelector("header").classList.add()
+//   //   }
+//   // })
+
+const hed_btn = document.querySelector(".burg");
+
+hed_btn.addEventListener("click", function () {
+  hed_btn.classList.toggle("is-active");
+});
+// };
